24f67b528948584e84f804ecb0cdd9f51638e205
Sorochan Ilya <>
2023-09-03 20:15:30 +0300

    Add shadowsocks and outline support

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - Kitty colorcheme can be configured via pywal:
   + `wal -i path/to/image`
   + `cp ~/.cache/wal/kitty-colors.conf ~/.config/kitty/.`
-  + `echo 'include ~/.cache/wal/colors-kitty.conf' > ~/.config/kitty/kitty.conf`
+  + `echo 'include ~/.config/kitty/colors-kitty.conf' > ~/.config/kitty/kitty.conf`
   + same can be done for waybar (copy `colors-waybar.css`)
 - Helix: `echo "theme = 'dark_plus'" > ~/.config/helix/config.toml`
 - Firefox configure
@@ -16,6 +16,10 @@
   + `git config --global user.name "Your Name"`
   + Ssh keygen: `ssh-keygen -t ed25519 -C "your_email@example.com"`
   + Then add `key123.pub` to your account (github site)
+- Also shadowsocks-rust is included in this build
+  + If you want to use it together with outline then there is `ss.py`:
+  + `python ss.py /path/to/keyfile`
+  + key file is just `ssconf` link
 
 ## Cleanup
 
--- configuration.nix
+++ configuration.nix
@@ -161,11 +161,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # media-session.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -173,8 +173,6 @@
     isNormalUser = true;
     description = "k0tran";
     extraGroups = [ "networkmanager" "wheel" ];
-    # packages = with pkgs; [
-    # ];
   };
 
   # Allow unfree packages
@@ -273,36 +271,25 @@
 
     # Dev tools
     git
+    docker
     clang
     python3Full
     python2
+    rustup
     zig
     zls
+    gef # advanced gdb
 
     # System tools
     killall
     file
     wget
+
+    # Shadowsocks
+    shadowsocks-rust
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  virtualisation.docker.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
